5e455e78b019961d355dcb61ad5b3e2445a12410
harshpatel6553-tech <harshpatel6553-tech@users.noreply.github.com>
2026-06-10 09:34:10 +0530

    fix: resolve typescript errors in newsService to pass production build

--- server/src/services/newsService.ts
+++ server/src/services/newsService.ts
@@ -1,6 +1,6 @@
 import { XMLParser } from 'fast-xml-parser';
 import * as he from 'he';
-import { logger } from '../utils/logger.js';
+import logger from '../utils/logger.js';
 
 export interface NewsItem {
   id: string;
