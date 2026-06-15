7430cbd0fea12a6436c2006d7a180fc1de11637a
harshpatel6553-tech <harshpatel6553-tech@users.noreply.github.com>
2026-06-15 13:02:44 +0530

    fix: add tweet_mode=extended to RapidAPI request to force full untruncated text

--- server/src/services/newsService.ts
+++ server/src/services/newsService.ts
@@ -42,7 +42,7 @@ class NewsService extends EventEmitter {
       for (let i = 0; i < keys.length; i++) {
         const apiKey = keys[this.currentKeyIndex % keys.length];
         try {
-          response = await fetch('https://twitter-search-only.p.rapidapi.com/timeline.php?screenname=RedboxIndia', {
+          response = await fetch('https://twitter-search-only.p.rapidapi.com/timeline.php?screenname=RedboxIndia&tweet_mode=extended', {
             headers: {
               'x-rapidapi-key': apiKey,
               'x-rapidapi-host': 'twitter-search-only.p.rapidapi.com'
